8a5b5dc7e8179e2cf0ad3c949e54791874f6bc9d
PDXChris <chris.prescott@portlandoregon.gov>
2015-12-17 17:18:49 -0800

    Fix comma error.

--- R/fixFishNames.r
+++ R/fixFishNames.r
@@ -31,8 +31,8 @@ fixFishNames <- function(vct) {
                  "reticulate sculpin"       = "^reticultate sculpin$",
                  "pacific giant salamander" = "dicamptodon.*",
                  "cutthroat trout"          = "cutthroat.*",
-                 "peamouth"                 = "peamouth.*"
-                 "sand roller"              = "sandroller"
+                 "peamouth"                 = "peamouth.*",
+                 "sand roller"              = "sandroller",
                  "unidentified centrarchid" = "unidentified sunfish/bass")
 
   ret <- stri_replace_all_regex(vct, pattern = unname(kNamesMap),
