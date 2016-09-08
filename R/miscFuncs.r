a2c873f4310d997c1350cadcb235f5250411002d
PDXChris <chris.prescott@portlandoregon.gov>
2016-09-08 14:02:23 -0700

    Add statByStation.

--- R/miscFuncs.r
+++ R/miscFuncs.r
@@ -43,3 +43,25 @@ loadPMdat <- function(dtype) {
 gmean <- function(x){
   exp(mean(log(x)))
 }
+
+#' Calculate summary statistic by station
+#' @param dfm data frame containing the data
+#' @param stat statistical function to calculate.  Mean is the default.
+#' @param station station field name, supplied as a string
+#' @param vbl field name numeric variable to summarize, supplied as a string
+#' @return a data frame with summary statistic by station
+#' @import plyr
+#' @export
+
+statByStation <- function(dfm, stat = 'mean', station = 'loc_code',
+                          vbl = 'result') {
+  statByStat_1 <- function(x) {
+    stat = do.call(stat, list(x[['result']]))
+    cns = ifelse(any(x[['cens']]=='='), 'Detected', 'None\nDetected')
+    pct_det = mean(x$cens == '=')
+    data.frame(stat, cns, pct_det)
+  }
+
+  ddply(dfm, station, statByStat_1)
+}
+
