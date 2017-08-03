ef959962104399d4bad901f97c6b2ce128a26d5b
Chris Prescott <chris.prescott@portlandoregon.gov>
2017-08-02 18:20:16 -0700

    make mergeStatInfo default to station as the merge field.  Add missing @param to several functions.

--- R/mergeStatInfo.r
+++ R/mergeStatInfo.r
@@ -1,22 +1,23 @@
 #' Merge PAWMAP station information into a data frame.
 #'
 #' @param df  A data frame with a station field
-#' @param stat  The station field within df
+#' @param by.x  The station field within the stationInfo table
+#' @param by.y  The station field within the data frame df
 #' @param fields  Fields to add from the station table
 #' @return The original data frame merged with station fields.  The merge
-#' returns all rows of df even if there is no match to indicate there
+#' returns all rows of df even if there is no match, to indicate that there
 #' are stations in df not present in the station table.
 #' @examples
-#' df <- data.frame(loc_code=unique(stationInfo$loc_code),
-#'                  result=rnorm(length(stationInfo$loc_code)))
+#' df <- data.frame(loc_code=unique(stationInfo$station),
+#'                  result=rnorm(length(stationInfo$station)))
 #' mergeStatInfo(df)
 #' @export
 
 
-mergeStatInfo <- function(df, stat='loc_code',
+mergeStatInfo <- function(df, by.x='station', by.y='station',
                           fields=c('watershed', 'subwat',
                                    'loc.lbl', 'panel', 'duration')) {
-  merge(stationInfo[,c(stat, fields)],
+  merge(stationInfo[, c(by.x, fields)],
         df[, !colnames(df) %in% fields],
-        by = stat, all.y=TRUE)
+        by.x=by.x, by.y=by.y, all.y=TRUE)
 }
