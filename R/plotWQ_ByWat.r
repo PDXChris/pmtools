aef5b38c630ccc265a7076f5129446e1a804b882
Chris Prescott <chris.prescott@portlandoregon.gov>
2018-05-15 22:25:50 -0700

    genericize plotWQ_ByWat

--- R/plotWQ_ByWat.r
+++ R/plotWQ_ByWat.r
@@ -2,6 +2,8 @@
 #'
 #' @param dfm  The data frame containing the variable
 #' @param result  the name of the water quality variable to plot
+#' @param analyte_field the field containing the name of the analyte
+#' @param analyte_units the field containing the analyte measurement units
 #' @return A ggplot box plot of the variable by watershed
 #' @import ggplot2
 #' @examples
@@ -16,7 +18,8 @@
 #' @export
 
 
-plotWQ_ByWat <- function(dfm, result = 'result') {
+plotWQ_ByWat <- function(dfm, result = 'result', analyte_field='janus_analyte_name',
+                         analyte_units='analyte_units') {
 
   # merge w/ station info; add storm field
   dfm <- mergeStatInfo(dfm)
@@ -29,9 +32,10 @@ plotWQ_ByWat <- function(dfm, result = 'result') {
                                    "Johnson\nCreek", "Columbia\nSlough"))
   breaks <- as.vector(c(1, 2, 5) %o% 10^(-5:5))
 
-  vlbl <- met.cod$label[match(vbl, met.cod[, vName])]
+  vbl <- unique(dfm[[analyte_field]])
+  vlbl <- met.cod$label[match(vbl, met.cod[, 'metric_name'])]
   trim.trailing <- function (x) sub("\\s+$", "", x)
-  ylb <- paste0(vlbl,' (', trim.trailing(unique(dfm$units)), ')\n')
+  ylb <- paste0(vlbl,' (', trim.trailing(unique(dfm[[analyte_units]])), ')\n')
 
 
   p <- ggplot(data=dfm, aes_string('watershed', result, fill='storm')) + geom_boxplot() +
@@ -43,14 +47,14 @@ plotWQ_ByWat <- function(dfm, result = 'result') {
                           axis.text.x  = element_text(size=15)) +
     geom_vline(xintercept = c(2.5, 4.5), size=1)
 
-  lbl <- data.frame(x=c(1.5, 3.5, 5.5), y=rep(1.4*max(dfm$result), 3),
+  lbl <- data.frame(x=c(1.5, 3.5, 5.5), y=rep(1.4*max(dfm[[result]]), 3),
                     label=c('West Slope of\nWest Hills',
                             'East Slope of\nWest Hills', 'Eastside\nStreams'))
   p <- p + geom_text(aes(x=x, y=y, label=label, fill=NULL, face='bold'),
                      size=(5), vjust=1, data=lbl)
 
   # provide standard lines where available
-  m.tmp <- as.character(met.cod$metric_code[match(vbl, met.cod[, vName])])
+  m.tmp <- as.character(met.cod$metric_code[match(vbl, met.cod[, 'metric_name'])])
   if (m.tmp %in% std.lns$met.cod) {
     r.lin <- std.lns[match(m.tmp, std.lns$met.cod), ]$red.line
     if (m.tmp == 'do' | m.tmp == 'ecoli') {
