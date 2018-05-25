d40a17ec19bcd6f216aa940f8319a2eb6e7b3272
Chris Prescott <chris.prescott@portlandoregon.gov>
2018-05-25 13:40:11 -0700

    plotHab_ByWat: change labeling & clean up org a bit.

--- R/plotHab_ByWat.r
+++ R/plotHab_ByWat.r
@@ -1,38 +1,41 @@
 #' Plot PAWMAP habitat data by watershed.
 #'
-#' @param vbl  the name of the water quality variable to plot
+#' @param vbl  the field containing the variable to plot
 #' @param dfm  The data frame containing the variable
 #' @return A ggplot box plot of the variable by watershed
 #' @examples
 #' library(ggplot2)
+#' ## NEED TO UPDATE
 #' d <- data.frame(loc_code=unique(stationInfo$loc_code), metric_code='xcl',
 #'                  result=rnorm(length(stationInfo$loc_code)))
 #' p <- plotHab_ByWat('xcl', d)
 #' p + ggtitle('Large Riparian Canopy - Generated Data for Example\n')
 #' @export
 
-plotHab_ByWat <- function(vbl, dfm=hab14) {
-
-  # Subset data to single variable, format
-  dfm <- dfm[dfm[, 'metric_code'] == vbl, ]
+plotHab_ByWat <- function(vbl, dfm) {
 
   # Add more descriptive variable info - location, labels, title
   tmp <- mergeStatInfo(dfm)
-  tmp <- merge(tmp, met.cod, by='metric_code')
+
+  # Create labels
+  lbls <- as.character(met.cod[match(vbl, met.cod$metric_code)])
+
   tmp$watershed <- gsub(' ', '\n', tmp$watershed)
   tmp$watershed <- factor(tmp$watershed,
                           levels=c("Fanno\nCreek", "Tualatin\nStreams",
                                    "Tryon\nCreek", "Willamette\nStreams",
                                    "Johnson\nCreek", "Columbia\nSlough"))
 
-  ttl <- paste0(unique(tmp$short), ' in Portland Streams\n')
+  ttl <- paste0(poll.lab$label, ' in Portland Streams\n')
 
-  p <- ggplot(aes(watershed, result), data=tmp) + geom_boxplot(fill='darkseagreen') + theme_bw() +
-    xlab('') + ylab(paste0('\n', unique(tmp$label),'\n')) +
+  p <- ggplot(aes_string('watershed', vbl), data=tmp) +
+    geom_boxplot(fill='darkseagreen') +
     scale_fill_discrete('Type', labels=c('Seasonal', 'Storm')) +
+    scale_y_continuous(expand=c(0.1, 0)) +
+    geom_vline(xintercept=c(2.5, 4.5), size=1) +
+    theme_bw() + xlab('') + ylab(paste0('\n', lbls$units,'\n')) +
     ggtitle(ttl) + theme(plot.title = element_text(size=16, face='bold'),
-                         axis.text.x  = element_text(size=12)) +
-    geom_vline(xintercept=c(2.5, 4.5), size=1) + scale_y_continuous(expand=c(0.1, 0))
+                         axis.text.x  = element_text(size=12))
 
   if (vbl=="v1tm100") {
     p <- p + geom_hline(y=30, color='darkgreen', size=1.2) +
@@ -49,7 +52,7 @@ plotHab_ByWat <- function(vbl, dfm=hab14) {
                     label=c('West Slope of\nWest Hills',
                             'East Slope of\nWest Hills', 'Eastside\nStreams'))
 
-  p <- p + geom_text(aes(x=x, y=y, label=label, fill=NULL, face='bold'), data=lbl)
+  p <- p + geom_text(data=lbl, aes(x=x, y=y, label=label, fill=NULL, face='bold'))
 
   return(p)
 }
