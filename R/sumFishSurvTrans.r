82b22e2289c50131a74e252d225f9cdac946bd15
Chris Prescott <chris.prescott@portlandoregon.gov>
2022-01-28 20:15:12 -0800

    Create sumFishSurvTrans.r
    create sumFishSurvTrans

---
+++ R/sumFishSurvTrans.r
@@ -0,0 +1,27 @@
+#' Query number of PAWMAP fish surveys and transects.
+#'
+#' @import RODBC, dplyr, BESdata
+#' @export
+
+sumFishSurvTrans <- function(){
+
+  # query data on number of surveys & transects
+  con <- BESdata:::dbConnect("WATERSHED")
+  event <- sqlQuery(con, "select * from fish_event;")
+  sites <- sqlQuery(con, "select * from site;")
+  fishDB <- merge(sites, event, by = 'site_id')
+
+  # tally transects per date; plot counts
+  numTrans <- fishDB %>% group_by(site_identifier, collection_end) %>%
+    summarise(num_trans= sum(c_across(subreach_ab_fished:subreach_jk_fished), na.rm=T))
+
+  h <- hist(numTrans$num_trans, plot=F)
+
+  plot(h, ylim = c(0, max(h$counts)*1.2), xlab = '# Transects Sampled per Survey',
+       main = 'PAWMAP Fish Surveys', labels=T)
+
+  numSurv <- numTrans %>% summarise(num_survs = n(), totNumTrans = sum(num_trans))
+
+  fishSurvTrans <- list(numSurv=numSurv, numTrans=numTrans, plot=h)
+  fishSurvTrans
+}
