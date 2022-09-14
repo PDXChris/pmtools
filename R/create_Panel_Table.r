21314ff3dd51feb9761c9fe32b83dd6b2133510b
Chris Prescott <chris.prescott@portlandoregon.gov>
2022-09-14 13:34:42 -0700

    delete old functions
    delete old functions; fix create_Panel_Table

--- R/create_Panel_Table.r
+++ R/create_Panel_Table.r
@@ -6,8 +6,9 @@
 
 create_Panel_Table <- function(cycles = 5){
 
-  panel_years <- data.frame(PAWMAP_Year = 1:(n*4), TempYear = 2010:(2010 + n*4 - 1),
-                            panel = rep(c(1:4), n), cycle = rep(1:n, each = 4))
+  panel_years <- data.frame(PAWMAP_Year = 1:(cycles * 4),
+                            TempYear = 2010:(2010 + cycles * 4 - 1),
+                            panel = rep(c(1:4), cycles), cycle = rep(1:cycles, each = 4))
 
   panel_years$FiscalYear <- paste0(panel_years$tempYear, '-', panel_years$tempYear + 1)
 
